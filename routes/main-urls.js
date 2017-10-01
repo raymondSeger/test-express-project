d3f918bbeaa1972ac5dbe67bce6b40d44764379f
Raymond Seger <raymondseger@live.com>
2017-10-01 20:06:44 +0700

    use chalk for colored text in console.log
    use chalk for colored text in console.log

--- routes/main-urls.js
+++ routes/main-urls.js
@@ -12,6 +12,7 @@ const compression   = require('compression');
 const uuidv4        = require('uuid/v4');
 const _             = require('underscore');
 const cors          = require('cors');
+const chalk         = require('chalk');
 const calculate     = require('../my_functions/calculate');
 
 // favicon
@@ -38,6 +39,42 @@ router.use(function timeLog2 (req, res, next) {
 router.get('/', function (req, res) {
     res.render('main', { title: 'Hey', message: 'Hello there!' });
 });
+
+router.get('/useChalk', function (req, res) {
+    let log = console.log;
+
+    // Combine styled and normal strings
+    log(chalk.blue('Hello') + 'World' + chalk.red('!'));
+
+    // Compose multiple styles using the chainable API
+    log(chalk.blue.bgRed.bold('Hello world!'));
+
+    // Pass in multiple arguments
+    log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+
+    // Nest styles
+    log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+
+    // Nest styles of the same type even (color, underline, background)
+    log(chalk.green(
+        'I am a green line ' +
+        chalk.blue.underline.bold('with a blue substring') +
+        ' that becomes green again!'
+    ));
+
+    // ES2015 template literal
+    log(`
+    CPU: ${chalk.red('90%')}
+    RAM: ${chalk.green('40%')}
+    DISK: ${chalk.yellow('70%')}
+    `);
+
+    // Use RGB colors in terminal emulators that support it.
+    log(chalk.keyword('orange')('Yay for orange colored text!'));
+    log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
+    log(chalk.hex('#DEADED').bold('Bold gray!'));
+        res.send( uuidv4() );
+    });
 
 router.get('/useUUID', function (req, res) {
     res.send( uuidv4() );
